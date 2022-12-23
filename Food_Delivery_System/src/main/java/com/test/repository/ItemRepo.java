87feb2cdd8f4858e5f5b750defe691044e079951
Isha Vats <111346925+Isha1906@users.noreply.github.com>
2022-12-23 17:58:58 +0530

    Delete Food_Delivery_System directory

--- Food_Delivery_System/src/main/java/com/test/repository/ItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.test.model.Cart;
-
-public interface ItemRepo extends JpaRepository<Cart, Integer> {
-
-}
